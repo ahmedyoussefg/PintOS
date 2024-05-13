dca4d147b053e73b7662c437cadd81abec9b0795
ahmedyoussefg <a.goerany@gmail.com>
2024-05-13 15:15:24 +0300

    put syscall_handler basic structure for wait and exit

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,19 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
+  int sys_call_type = (int)f->esp;
+  if (sys_call_type == SYS_WAIT){
+    // handle wait
+  }
+  else if (sys_call_type == SYS_EXIT) {
+    // handle exit
+  }
+  else if (sys_call_type == SYS_EXEC){
+    // handle exec
+  }
+  //  TODO: ....continue if elses........ for remaining  syscalls
   thread_exit ();
 }
