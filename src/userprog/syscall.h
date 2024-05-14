9fe408701833749965067c1daa0c03bd5eb74e5e
ahmedyoussefg <a.goerany@gmail.com>
2024-05-15 00:44:46 +0300

    fix syntax

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,15 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include "threads/interrupt.h"
 
+typedef int pid_t;
 void syscall_init (void);
+void validate_void_ptr(void *ptr);
+void wait_wrapper (struct intr_frame *f);
+int wait(pid_t pid);
+void halt_wrapper(void);
+void halt(void);
+void exit_wrapper(struct intr_frame *f);
+void exit(int status);
 
 #endif /* userprog/syscall.h */
